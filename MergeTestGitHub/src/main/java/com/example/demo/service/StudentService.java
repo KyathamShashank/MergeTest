e36c37467f5cb1af96fd03841990926bf33e5e7e
Shashank <kyathamshashank1723@gmail.com>
2024-11-12 12:51:36 +0530

    main push 4

--- MergeTestGitHub/src/main/java/com/example/demo/service/StudentService.java
+++ MergeTestGitHub/src/main/java/com/example/demo/service/StudentService.java
@@ -4,7 +4,5 @@ public class StudentService {
 void StudentId()
 {
 	System.out.println("student id is "+10);
-	System.out.println("this is for b1 branch");
-	System.out.println("this is for branch1 ");
 }
 }
