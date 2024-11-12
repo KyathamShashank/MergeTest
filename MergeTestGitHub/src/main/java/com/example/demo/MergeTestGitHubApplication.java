9aabf63c0dbcbf57de12b189c2360221cf777e75
Shashank <kyathamshashank1723@gmail.com>
2024-11-12 12:30:09 +0530

    1 commit

---
+++ MergeTestGitHub/src/main/java/com/example/demo/MergeTestGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeTestGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeTestGitHubApplication.class, args);
+	}
+
+}
